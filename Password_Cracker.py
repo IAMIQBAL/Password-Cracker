c2bb4895ce4188e2283dbc647597a54d3ee0de38
MUHAMMAD IQBAL <muhammadiqbal2015@hotmail.com>
2018-10-30 02:06:09 +0500

    Add files via upload

---
+++ Password_Cracker.py
@@ -0,0 +1,26 @@
+import crypt
+
+def crackPassword(cryptPass):
+
+	salt = cryptPass[0:2]
+
+	passwords = open('dictionary.txt', 'r')
+	for pwds in passwords.readlines():
+		pwds = pwds.strip('\n')
+		cryptPwds = crypt.crypt(pwds, salt)
+		if (cryptPwds == cryptPass)
+			print("Found Password: " + pwds)
+	print("Password not Found")
+	return
+
+
+def main():
+	passFile = open('') #Path of the file containing users and crypts
+	for l in passFile.readlines():
+		if ":" in line:
+			user = line.split(':')[0]
+			cryptPass = line.split(':')[1].strip(' ')
+			print("Cracking Password For: " + user)
+
+if __name__ == "__main__":
+	main()
